91e72c9690a2910aa29db9eff93d61ce10d088f1
chashokmca <chashokmca@gmail.com>
2023-05-20 14:48:07 +0530

    Initial commit

--- common.sh
+++ common.sh
@@ -1,25 +1,26 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_name=$(dirname "$script")
-log_file=/tmp/roboshop.log
+log_file=/tmp/${app_user}.log
 #source $script_name/common.sh
 
 function_print_header() {
 	echo -e "\e[33m<<<<<<<<<<  $1 >>>>>>>>>>\e[0m"
+	echo -e "\e[33m<<<<<<<<<<  $1 >>>>>>>>>>\e[0m" &>>$log_file
 }
 
 
 function_java() {
 	function_print_header "configure nodejs repos"
-	yum install maven -y
+	yum install maven -y &>>$log_file
 	function_status_check $?
 	
 	function_app_config
 	
 	function_print_header "clean and install maven package"
 	cd /app 
-	mvn clean package 
-	mv target/shipping-1.0.jar shipping.jar
+	mvn clean package &>>$log_file
+	mv target/shipping-1.0.jar shipping.jar &>>$log_file
 	function_status_check $?
 	
 	func_schema_setup
@@ -28,17 +29,17 @@ function_java() {
 
 function_nodejs() {
 	function_print_header "configure nodejs repos"
-	curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+	curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 	function_status_check $?
 
 	function_print_header "install nodejs"
-	yum install nodejs -y
+	yum install nodejs -y &>>$log_file
 	function_status_check $?
 	
 	function_app_config
 	
 	function_print_header "download dependencies"
-	npm install
+	npm install &>>$log_file
 	function_status_check $?
 	
 	func_schema_setup
@@ -47,63 +48,63 @@ function_nodejs() {
 
 function_start_component() {
 	function_print_header "copy catalogue service"
-	cp ${script_name}/${component}.service /etc/systemd/system/${component}.service
+	cp ${script_name}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
 	function_status_check $?
 
 	function_print_header "start catalogue service"
-	systemctl daemon-reload
-	systemctl enable ${component} 
-	systemctl start ${component}
+	systemctl daemon-reload &>>$log_file
+	systemctl enable ${component} &>>$log_file
+	systemctl start ${component} &>>$log_file
 	function_status_check $?
 }
 
 function_app_config() {
 	function_print_header "add application user"
-	id roboshop
+	id ${app_user}
 	if [$? -ne 0]; then
-		useradd roboshop
+		useradd ${app_user} &>>$log_file
 	else 
-		echo -e "User Already present"
+		echo -e "User Already present" &>>$log_file
 	fi
 	function_status_check $?
 
 	function_print_header "create application directory"
-	rm -rf /app
-	mkdir /app 
+	rm -rf /app &>>$log_file
+	mkdir /app &>>$log_file
 	function_status_check $?
 
 	function_print_header "download application"
-	curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+	curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}. &>>$log_file
 	function_status_check $?	
 
 	function_print_header "extract application"
-	cd /app 
-	unzip /tmp/${component}.zip
+	cd /app &>>$log_file
+	unzip /tmp/${component}.zip &>>$log_file
 	function_status_check $?
 }
 
 func_schema_setup() {
 	if [ ${schema_setup} == "mongo" ]; then
 		function_print_header "configure mongo db repo"
-		cp ${script_name}/mongo.repo /etc/yum.repos.d/mongo.repo
+		cp ${script_name}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 		function_status_check $?
 
 		function_print_header "install mongo db client"
-		yum install mongodb-org-shell -y
+		yum install mongodb-org-shell -y &>>$log_file
 		function_status_check $?
 
 		function_print_header "configure application schema"
-		mongo --host mongodb-dev.adevops72.online </app/schema/${component}.js
+		mongo --host mongodb-dev.adevops72.online </app/schema/${component}.js &>>$log_file
 		function_status_check $?
 	fi
 	
 	if [ ${schema_setup} == "mysql" ]; then
 		function_print_header "install mysql db client"
-		yum install mysql -y 
+		yum install mysql -y &>>$log_file
 		function_status_check $?
 		
 		function_print_header "install mysql schema"
-		mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+		mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$log_file
 		function_status_check $?		
 	fi
 	
@@ -113,14 +114,14 @@ func_schema_setup() {
 function_php() {
 	
 	function_print_header "install python and it's dependencies"
-	yum install python36 gcc python3-devel -y
+	yum install python36 gcc python3-devel -y &>>$log_file
 	function_status_check $?
 	
 	function_app_config
 	
 	function_print_header "install python requirements"
-	cd /app 
-	pip3.6 install -r requirements.txt
+	cd /app &>>$log_file
+	pip3.6 install -r requirements.txt &>>$log_file
 	function_status_check $?
 	
 	function_start_component
@@ -129,16 +130,16 @@ function_php() {
 function_golang() {
 
 	function_print_header "install golang"
-	yum install golang -y
+	yum install golang -y &>>$log_file
 	function_status_check $?
 	
 	function_app_config
 	
 	function_print_header "Build application"
-	cd /app 
-	go mod init dispatch
-	go get 
-	go build
+	cd /app &>>$log_file
+	go mod init dispatch &>>$log_file
+	go get &>>$log_file
+	go build &>>$log_file
 	function_status_check $?
 	
 	function_start_component
@@ -149,5 +150,6 @@ function_status_check() {
 		echo -e "\e[32mSUCCESS\e[0m"
 	else	
 		echo -e "\e[32mFAILURE\e[0m"
+		echo -e "please refer ${log_file} for more information"
 	fi
 }	
